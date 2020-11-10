118894fcb16b64466082a77a96de3c64ec1037e0
ladni <ladnik@hotmail.com>
2020-11-10 08:14:46 +0200

    Remove extra files copy

--- nestjs-graphql-service/__COPY__.cmd
+++ nestjs-graphql-service/__COPY__.cmd
@@ -1,5 +1,3 @@
-xcopy .\rpc-context-creator.js .\node_modules\@nestjs\microservices\context\* /y
-
 xcopy .\dist\modules\gql.module.js ..\file-server\gql\* /y
 xcopy .\schema.gql ..\file-server\gql\* /y
 xcopy .\requestPipeline.js  ..\libs\gql-module-lib\node_modules\apollo-server-express\node_modules\apollo-server-core\dist\* /y

7837d076951a8fd9a76327f25829f154bf93b4a5
ladni <ladnik@hotmail.com>
2020-12-10 12:53:40 +0200

    Change for proper install and build libs

---
+++ libs/_BUILD_LIBS.cmd
@@ -0,0 +1,41 @@
+rem Install
+cd common-utils-lib
+cmd /c npm i
+cd ..
+
+cd config-lib
+cmd /c npm i
+cd ..
+
+cd logger-lib
+cmd /c npm i
+cd ..
+
+cd module-loader-lib
+cmd /c npm i
+cd ..
+
+cd interceptors-lib
+cmd /c npm i
+cd ..
+
+cd sql-base-lib
+cmd /c npm i
+cd ..
+
+cd auth-lib
+cmd /c npm i
+cd ..
+
+cd login-lib
+cmd /c npm i
+cd ..
+
+cd gql-module-lib
+cmd /c npm i
+cd ..
+
+
+
+
+
